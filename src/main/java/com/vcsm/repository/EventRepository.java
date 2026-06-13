33bb18ffaf61b404823162218e9fae2834bcbef7
deepikavashishthaa665 <deepikavashishtha665@gmail.com>
2026-06-13 15:12:51 +0530

    Fix repository methods and add gitignore

--- src/main/java/com/vcsm/repository/EventRepository.java
+++ src/main/java/com/vcsm/repository/EventRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findByActiveTrue();
+    List<Event> findByCategoryAndActiveTrue(Event.EventCategory category);
 
     List<Event> findByEventDateAfterAndActiveTrue(LocalDateTime dateTime);
 }
